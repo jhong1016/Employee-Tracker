fff0a87020e75bdc3b8a684b255472515f4503a0
Jin Hong <jnhg1016@gmail.com>
2021-05-18 23:44:04 +1000

    roles added to seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -11,3 +11,26 @@ INSERT INTO department (id, name)
 VALUES (4, "Legal");
 INSERT INTO department (id, name)
 VALUES (5, "IT");
+
+----- Role Seeds -----
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 45000, 1);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Salesperson", 35000, 1);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Assistant Store Manager", 60000, 1);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Store Manager", 75000, 1);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Lead Engineer", 43000, 2);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "Accountant", 50000, 3);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "Lawyer", 40000, 4);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "System Adminstrator", 80000, 5);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "IT Coordinator", 52000, 5);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (10, "IT Manager", 105000, 5);
+
