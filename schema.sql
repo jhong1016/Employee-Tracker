707d6d5f6d5ec45d7981a6a182bbb3e166ff3edc
Jin Hong <jnhg1016@gmail.com>
2021-05-18 23:15:38 +1000

    create role table added to schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,22 @@
-DROP DATABASE IF EXISTS employees_DB;
+DROP DATABASE IF EXISTS employee_DB;
 
-CREATE DATABASE employees_DB;
+CREATE DATABASE employee_DB;
 
-USE employees_DB;
+USE employee_DB;
 
------ Create Department table -----
+----- Create Department Table -----
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
+);
+
+----- Create Role Table -----
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+	PRIMARY KEY (id)
 );
