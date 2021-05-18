59942d179c8f9de5114ee22662607e333e04831f
Jin Hong <jnhg1016@gmail.com>
2021-05-18 23:05:48 +1000

    options added to prompt user with choices

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ connection.connect((err) => {
     console.log("\n Welcome to Employee Tracker \n");
     options();
 });
+
+// Prompt user to choose an option
+function options() {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            "View all employees",
+            "View all employees by role",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add employee",
+            "Add role",
+            "Add department",
+            "Update employee role",
+            "Update employee manager",
+            "Delete employee",
+            "Delete role",
+            "Delete department",
+            "View department budgets"
+          ]
+        })
+    }
