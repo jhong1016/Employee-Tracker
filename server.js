b33bc7fd0f34b427d0a5e98c2411165c799f55ca
Jin Hong <jnhg1016@gmail.com>
2021-05-20 14:29:58 +1000

    added funcitons to build tables

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const db = require('./utils/connection.js');
+const runApp = require('./utils/questions.js');
 const consoleTable = require('console.table');
 const promisemysql = require('promise-mysql');
 const logo = require("asciiart-logo");
@@ -19,6 +21,77 @@ function displayLogo() {
         .render()
     );
 }
+
+// // Main Menu - Prompt user to choose an option
+function runApp() {
+    inquirer.prompt({
+        name: "mainmenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Edit Employee Info",
+            "View Roles",
+            "Edit Roles",
+            "View Departments",
+            "Edit Departments"
+        ]
+    }).then(responses => {
+        // Switch case depending on user option
+        switch (responses.mainmenu) {
+            case "View All Employees":
+                showEmployeeSummary();
+                break;
+            case "Edit Employee Info":
+                editEmployeeOptions();
+                break;
+            case "View Roles":
+                showRoleSummary();
+                break;
+            case "Edit Roles":
+                editRoleOptions();
+                break;
+            case "View Departments":
+                showDepartments();
+                break;
+            case "Edit Departments":
+                editDepartmentOptions();
+                break;
+        }
+    });
+}
+
+runApp();
+
+// Builds complete employee table
+async function showEmployeeSummary() {
+    console.log(' ');
+    await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runApp();
+    });
+};
+
+// Builds a table which shows existing roles and their departments
+async function showRoleSummary() {
+    console.log(' ');
+    await db.query('SELECT r.id, title, salary, name AS department FROM role r LEFT JOIN department d ON department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runApp();
+    })
+};
+
+// Builds a table which shows existing departments
+async function showDepartments() {
+    console.log(' ');
+    await db.query('SELECT id, name AS department FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runApp();
+    })
+};
 
 // View all employees in the database
 function viewAllEmp(){
