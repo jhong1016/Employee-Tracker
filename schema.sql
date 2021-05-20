51cbfdfc1476cdc78367bf72ff9eb56402f4d836
Jin Hong <jnhg1016@gmail.com>
2021-05-20 15:24:32 +1000

    schema.sql debugged

--- schema.sql
+++ schema.sql
@@ -6,30 +6,24 @@ USE employee_DB;
 
 ----- Create Department Table -----
 CREATE TABLE department (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 ----- Create Role Table -----
 CREATE TABLE role (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT auto_increment PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
-	PRIMARY KEY (id)
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INT NOT NULL
 );
 
 ----- Create Employee Table -----
 CREATE TABLE employee (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT auto_increment PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
-	PRIMARY KEY (id)
+    role_id INT NOT NULL,
+    manager_id INT
 );
 
